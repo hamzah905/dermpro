9752a1c5427d8c75bcabcbe398eeb309437ced48
Hamza Satti <hamza.satti159@gmail.com>
2020-03-03 17:33:42 +0500

    Remove sign up page

--- src/Components/Navbar/RightMenu.js
+++ src/Components/Navbar/RightMenu.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Modal, Menu, Button } from 'antd';
-import SignUp from './SignUp'
+import { Menu } from 'antd';
+// import SignUp from './SignUp'
 // const SubMenu = Menu.SubMenu;
 // const MenuItemGroup = Menu.ItemGroup;
 
@@ -34,15 +34,7 @@ class RightMenu extends Component {
           <a href="/">Signin</a>
         </Menu.Item>
         <Menu.Item key="app">
-          <Button onClick={this.showModal}>Signup</Button>
-          <Modal
-          title="Sign Up"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-        >
-          <SignUp/>
-        </Modal>
+          <a href="/">Signin</a>
         </Menu.Item>
       </Menu>
     );
